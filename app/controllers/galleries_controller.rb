baf8647475c845cd6f0693cbccc935c3d56587f7
Naga-Himaja <himajakeela18@gmail.com>
2025-03-11 02:25:39 +0530

    gallery: added controller and views

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,43 @@
 class GalleriesController < ApplicationController
+  def index
+    @galleries = Gallery.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.create(gallery_params)
+
+    redirect_to @gallery
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:id])
+    @gallery.update(artist_params)
+
+    redirect_to @gallery
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:id])
+    @gallery.destroy
+
+    redirect_to galleries_path
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:name, :location)
+  end
 end
