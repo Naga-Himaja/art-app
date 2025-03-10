df1301b503d7df6179611f8bf52c752ef34b0660
Naga-Himaja <himajakeela18@gmail.com>
2025-03-11 02:15:16 +0530

    fixed lint and rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :paintings, only: [:index, :show, :new, :create, :destroy]
+  resources :paintings, only: [ :index, :show, :new, :create, :destroy ]
   resources :galleries
   resources :artists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
